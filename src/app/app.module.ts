8ac46d695148b82b4d1ce5c69b83f91cf3784a0f
Abdelhadi Hussein <abod95@gmail.com>
2024-03-23 15:54:33 -0500

    Updated to latest cdk and material.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FrameModule } from './frame/frame.module';
 import { MatIconModule } from "@angular/material/icon";
 import { MatNativeDateModule } from '@angular/material/core';
-import { MatLegacySnackBarModule as MatSnackBarModule } from "@angular/material/legacy-snack-bar";
+import { MatSnackBarModule } from "@angular/material/snack-bar";
 import { NgxsRouterPluginModule } from '@ngxs/router-plugin';
 import { NgxsModule } from '@ngxs/store';
 import { ServiceWorkerModule } from '@angular/service-worker';
--- src/app/travel-salesman/travel-salesman.module.ts
+++ src/app/travel-salesman/travel-salesman.module.ts
@@ -2,15 +2,15 @@ import { NgModule } from "@angular/core";
 
 import { CommonModule } from "@angular/common";
 import { SharedModule } from "../shared";
-import { MatLegacyInputModule as MatInputModule } from '@angular/material/legacy-input';
-import { MatLegacyTableModule as MatTableModule } from '@angular/material/legacy-table';
-import { MatLegacySelectModule as MatSelectModule } from '@angular/material/legacy-select';
-import { MatLegacySnackBarModule as MatSnackBarModule } from '@angular/material/legacy-snack-bar';
+import { MatInputModule } from '@angular/material/input';
+import { MatTableModule } from '@angular/material/table';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { InterpreterRoutingModule } from "./travel-salesman-routing.module";
-import { MatLegacySlideToggleModule as MatSlideToggleModule } from '@angular/material/legacy-slide-toggle';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { HttpClientModule } from '@angular/common/http';
 import { TravelSalesmanComponent } from "./travel-salesman.component";
-import {MatLegacyChipsModule as MatChipsModule} from '@angular/material/legacy-chips';
+import {MatChipsModule} from '@angular/material/chips';
 
 @NgModule({
     declarations: [
